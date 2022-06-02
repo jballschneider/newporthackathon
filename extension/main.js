3423b1cc25d750e876762baecf1e4e3e54fd2a18
Oliver Shi <oshi@yext.com>
2022-06-02 12:23:01 -0400

    refactor

--- extension/main.js
+++ extension/main.js
@@ -1,24 +1,38 @@
-const identifiers = new Set([
-    "zdassets",
-    "force",
-    "livechat",
-    "now",
-    "aem",
-    "coveo",
-    "algolia",
-    "elastic",
-    "lucid",
-    "searchunify",
-    "demandware",
-    "uberall",
-    "where2buy"
-]);
+main();
 
-var links = document.querySelectorAll("link")
-links.forEach((link) => {
-    identifiers.forEach((identifier) => {
-        if (link.href.toLowerCase().includes(identifier)) {
-            console.log("found link href: ", link.href);
+function main() {
+    console.log('--- running yext extension')
+    const links = document.querySelectorAll('link');
+    links.forEach(link => checkAndReportUrl(link.href));
+    
+    const scriptTags = document.querySelectorAll('script')
+    scriptTags.forEach(scriptTag => checkAndReportUrl(scriptTag.src));
+}
+
+function checkAndReportUrl(url) {
+    if (!url) {
+        return
+    }
+    
+    const identifiers = new Set([
+        'zdassets',
+        'force',
+        'livechat',
+        'now',
+        'aem',
+        'coveo',
+        'algolia',
+        'elastic',
+        'lucid',
+        'searchunify',
+        'demandware',
+        'uberall',
+        'where2buy'
+    ]);
+
+    identifiers.forEach(identifier => {
+        if (url.toLowerCase().includes(identifier)) {
+            console.log('found link url: ', url)
         }
-    })
-})
+    });
+}
